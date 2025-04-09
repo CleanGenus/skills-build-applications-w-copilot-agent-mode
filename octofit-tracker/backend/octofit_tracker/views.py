5d72c2c9e38c5f69657074954023a29878c23f97
CleanGenus <JosephShao2233@outlook.com>
2025-04-09 12:42:20 +0000

    # step4 part 1 Setup Django REST Framework, restart the server, and test the API

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'http://localhost:8000/'
+    base_url = 'https://congenial-space-computing-machine-q7p4x5jp5ppv29556-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
